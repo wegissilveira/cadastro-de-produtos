b9ad86c7883109d42cabe4df97e786db01426e99
Wegis Silveira <wegis.h.silveira@gmail.com>
2021-08-28 17:43:55 -0300

    Criação de pasta imagens-demo

--- src/data/products_seed.js
+++ src/data/products_seed.js
@@ -41,6 +41,48 @@ const products_list = [
         valor: 10,
         valorTotal: 1000
     },
+    {
+        id: 7,
+        nome: 'Produto 7',
+        qtde: 5,
+        valor: 10,
+        valorTotal: 490
+    },
+    {
+        id: 8,
+        nome: 'Produto 8',
+        qtde: 39,
+        valor: 10,
+        valorTotal: 390
+    },
+    // {
+    //     id: 9,
+    //     nome: 'Produto 9',
+    //     qtde: 109,
+    //     valor: 20,
+    //     valorTotal: 2180
+    // },
+    // {
+    //     id: 10,
+    //     nome: 'Produto 10',
+    //     qtde: 50,
+    //     valor: 15,
+    //     valorTotal: 750
+    // },
+    // {
+    //     id: 11,
+    //     nome: 'Produto 11',
+    //     qtde: 499,
+    //     valor: 100,
+    //     valorTotal: 49000
+    // },
+    // {
+    //     id: 12,
+    //     nome: 'Produto 12',
+    //     qtde: 100,
+    //     valor: 10,
+    //     valorTotal: 1000
+    // },
 ]
 
 export default products_list
